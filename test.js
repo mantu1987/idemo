f9ceac7a0185154292b3f4babcf6758e99717456
mantu1987 <mantu.nigam@globallogic.com>
2016-01-20 15:48:33 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,38 @@
+ $(document).ready(function () {
+
+            $('#btnUploadFile').on('click', function () {
+
+                var data = new FormData();
+
+                var files = $("input[type='file']").get(0).files;
+
+                // Add the uploaded image content to the form data collection
+                if (files.length > 0) {
+                    data.append("UploadedImage", files[0]);
+                }
+
+                // Make Ajax request with the contentType = false, and procesDate = false
+                var ajaxRequest = $.ajax({
+                    type: "POST",
+                    url: "http://localhost:9000/upload",
+                    contentType: false,
+                    processData: false,
+                    data: data
+                });
+
+                ajaxRequest.done(function (responseData, textStatus) {
+                    if (textStatus == 'success') {
+                        if (responseData != null) {
+                            if (responseData.Key) {
+                                alert(responseData.Value);
+                                $("#fileUpload").val('');
+                            } else {
+                                alert(responseData.Value);
+                            }
+                        }
+                    } else {
+                        alert(responseData.Value);
+                    }
+                });
+            });
+        });
