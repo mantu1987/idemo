239c7881290dafe954cae0fc7f9fe68463367540
mantu1987 <mantu.nigam@globallogic.com>
2016-01-21 10:29:43 +0530

    test

--- test.js
+++ test.js
@@ -1,38 +1,58 @@
  $(document).ready(function () {
 
-            $('#btnUploadFile').on('click', function () {
+     $('#btnUploadFile').on('click', function () {
 
-                var data = new FormData();
+         var data = new FormData();
 
-                var files = $("input[type='file']").get(0).files;
+         var files = $("input[type='file']").get(0).files;
 
-                // Add the uploaded image content to the form data collection
-                if (files.length > 0) {
-                    data.append("UploadedImage", files[0]);
-                }
+         // Add the uploaded image content to the form data collection
+         if (files.length > 0) {
+             data.append("UploadedImage", files[0]);
+         }
 
-                // Make Ajax request with the contentType = false, and procesDate = false
-                var ajaxRequest = $.ajax({
-                    type: "POST",
-                    url: "http://localhost:9000/upload",
-                    contentType: false,
-                    processData: false,
-                    data: data
-                });
+         // Make Ajax request with the contentType = false, and procesDate = false
+         var ajaxRequest = $.ajax({
+             type: "POST",
+             url: "http://localhost:9000/upload",
+             contentType: false,
+             processData: false,
+             xhr: function () {
+                 var myXhr = $.ajaxSettings.xhr();
+                 if (myXhr.upload) {
+                     myXhr.upload.addEventListener('progress', function (e) {
+                         if (e.lengthComputable) {
+                             var max = e.total;
+                             var current = e.loaded;
 
-                ajaxRequest.done(function (responseData, textStatus) {
-                    if (textStatus == 'success') {
-                        if (responseData != null) {
-                            if (responseData.Key) {
-                                alert(responseData.Value);
-                                $("#fileUpload").val('');
-                            } else {
-                                alert(responseData.Value);
-                            }
-                        }
-                    } else {
-                        alert(responseData.Value);
-                    }
-                });
-            });
-        });
+                             var Percentage = (current * 100) / max;
+                             console.log(Percentage);
+
+
+                             if (Percentage >= 100) {
+                                 // process completed
+                             }
+                         }
+                     }, false);
+                 }
+                 return myXhr;
+             },
+             data: data
+         });
+
+         ajaxRequest.done(function (responseData, textStatus) {
+             if (textStatus == 'success') {
+                 if (responseData != null) {
+                     if (responseData.Key) {
+                         alert(responseData.Value);
+                         $("#fileUpload").val('');
+                     } else {
+                         alert(responseData.Value);
+                     }
+                 }
+             } else {
+                 alert(responseData.Value);
+             }
+         });
+     });
+ });
